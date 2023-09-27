c41d0b4b35f7b0d54555c0c0ff67125377f0c72c
Avilarc <nonoavawork@gmail.com>
2023-09-27 13:48:18 +0200

    Primera Redacción de ejercicios

---
+++ Redaccion1/ejercicio8.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Realiza un programa que genere números enteros aleatorios entre 0 y 10 y los muestre
+     * por pantalla, el programa terminará cuando el número sea 10. 
+     */
+
+    $numero = 0;
+
+     do {
+        $numero = rand(0,10);
+        echo 
+     } while ($numero != 10);
+?>
